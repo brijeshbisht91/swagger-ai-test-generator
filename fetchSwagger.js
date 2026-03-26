156667bbc3a0316ff4c3db92a777e4f66739296f
brijesh <brijesh.bisht91@gmail.com>
2026-03-26 19:50:35 +0530

    changed swagger.json file

--- fetchSwagger.js
+++ fetchSwagger.js
@@ -1,23 +1,27 @@
-const { default: axios } = require("axios")
-
 const fs = require('fs');
+const axios = require('axios');
 
 const SWAGGER_URL = 'https://petstore.swagger.io/v2/swagger.json';
 
-
 async function fetchSwagger() {
-    try {
-      const response = await axios.get(SWAGGER_URL);
-      
-      fs.writeFileSync(
-        'swagger.json',
-        JSON.stringify(response.data, null, 2)
-      );
-  
-      console.log("Swagger JSON saved successfully ✅");
-    } catch (error) {
-      console.error("Error fetching Swagger:", error.message);
+  try {
+    const response = await axios.get(SWAGGER_URL);
+
+    // अगर latest exist करता है → उसे prev बना दो
+    if (fs.existsSync('swagger-latest.json')) {
+      fs.copyFileSync('swagger-latest.json', 'swagger-prev.json');
     }
+
+    // नया swagger save करो
+    fs.writeFileSync(
+      'swagger-latest.json',
+      JSON.stringify(response.data, null, 2)
+    );
+
+    console.log("Swagger updated with versioning ✅");
+  } catch (error) {
+    console.error("Error fetching Swagger:", error.message);
   }
-  
-  fetchSwagger();
+}
+
+fetchSwagger();
